e49e7c123a6ad7031cb049f5828f757428939558
Hamersly <maksimwanderer96@gmail.com>
2021-11-16 18:46:24 +0500

    JsPrettier treatment

--- js/todo.js
+++ js/todo.js
@@ -1,39 +1,38 @@
-const field = document.querySelector('.field')
-const button = document.querySelector('.add')
+const field = document.querySelector(".field");
+const button = document.querySelector(".add");
 
-button.addEventListener('click', addTask);
+button.addEventListener("click", addTask);
 
 function creteTask(value) {
-	const checkboxInput = document.createElement('input')
-	const valueText = document.createElement('p')
-	const task = document.createElement("div")
+	const checkboxInput = document.createElement("input");
+	const valueText = document.createElement("p");
+	const task = document.createElement("div");
 
-	checkboxInput.type = 'checkbox';
-	checkboxInput.className = 'status';
-	checkboxInput.addEventListener('change', changeTask);
+	checkboxInput.type = "checkbox";
+	checkboxInput.className = "status";
+	checkboxInput.addEventListener("change", changeTask);
 
 	valueText.append(value);
 
-	task.className = 'task';
+	task.className = "task";
 	task.append(valueText, checkboxInput);
 	return task;
 }
 
 function changeTask(event) {
-	const {target} = event;
-	if(target.checked) {
-		target.parentElement.classList.add('success')
-	}else {
-		target.parentElement.classList.remove('success')
+	const { target } = event;
+	if (target.checked) {
+		target.parentElement.classList.add("success");
+	} else {
+		target.parentElement.classList.remove("success");
 	}
 }
 
 function addTask() {
-	if(field.value) {
-		const newTask = creteTask(field.value)
-		const list= document.querySelector('.list')
+	if (field.value) {
+		const newTask = creteTask(field.value);
+		const list = document.querySelector(".list");
 		list.appendChild(newTask);
-		field.value = '';
+		field.value = "";
 	}
 }
-
